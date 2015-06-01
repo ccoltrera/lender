d9e91f1a5d1a20d49227f2adfd83b53c03efdb37
Colin Coltrera <c.coltrera@gmail.com>
2015-06-01 13:00:40 -0700

    User creation, Item creation, Firebase integration Layout structure of app.js. Add functionality to create a user and add them to Firebase. Add functionality to create item, add its upc to local user inventory and Firebase inventory, and add it to Firebase.

---
+++ public/js/app.js
@@ -0,0 +1,74 @@
+var firebaseRef, usersRef, itemsRef, trackersRef;
+
+//Creates a Firebase() object, which links to Colin's Firebase
+lenderAppRef = new Firebase("http://boiling-torch-5679.firebaseIO.com");
+
+//Creates Firebase() objects, which link to relevant sub-nodes of the Firebase
+//data structure.
+usersRef = lenderAppRef.child("users");
+itemsRef = lenderAppRef.child("items");
+trackersRef = lenderAppRef.child("trackers");
+
+//User() object constructor.
+function User(userIdent) {
+  this.userIdent = userIdent;
+  //inventory array contains UPCs of Item() objects created and owned by the user.
+  this.inventory = [];
+  //ledger object contains arrays of transaction IDs
+  this.ledger = {
+    //lent array contains transaction IDs of Tracker() objects in which they are lender.
+    lent: [],
+    //borrowed array contains transaction IDs of Tracker() objects in which they are borrower.
+    borrowed: []
+  };
+  //Keeps track of the individual user's reference in the Firebase.
+  this.userRef = usersRef.child(this.userIdent);
+}
+
+//Creates a node in Firebase for the given user.
+User.prototype.initializeUser = function () {
+  this.userRef.child("userIdent").set(this.userIdent);
+}
+
+//Creates a new Item() object, adds it to the user's inventory browser side
+//and on Firebase.
+User.prototype.createItem = function(itemDetails) {
+  var upc, item;
+  upc = this.generateUPC(itemDetails);
+  item = new Item(this.userIdent, itemDetails, upc);
+  //Adds UPC to user's inventory
+  this.inventory.push(upc);
+  //Overwrites Firebase inventory with local inventory.
+  this.userRef.child("inventory").set(this.inventory);
+  //Adds item to Firebase
+  itemsRef.child(upc).set(item);
+
+}
+
+//Generates a UPC for item, using the itemDetails and the userIdent. Function can be
+//changed later for more advanced UPC generation.
+User.prototype.generateUPC = function(itemDetails) {
+  return this.userIdent + itemDetails;
+}
+
+User.prototype.initializeLend = function() {
+
+}
+
+User.prototype.confirmReturn = function() {
+
+}
+
+User.prototype.confirmBorrow = function() {
+
+}
+
+User.prototype.returnItem = function() {
+
+}
+
+function Item(owner, itemDetails, upc) {
+  this.owner = owner;
+  this.itemDetails = itemDetails;
+  this.upc = upc;
+}
