c870fc77f3bcbdb088c7421fcdecfb1fb449c54e
KateStearns <katestearns@mac.com>
2015-06-03 06:36:19 -0700

    login pulldown updates as more users are updated in firebase

---
+++ public/js/app.js
@@ -0,0 +1,175 @@
+var lenderAppRef, usersRef, itemsRef, trackersRef;
+
+//Creates a Firebase() object, which links to Colin's Firebase
+lenderAppRef = new Firebase("http://boiling-torch-5679.firebaseIO.com");
+
+//Creates Firebase() objects, which link to relevant sub-nodes of the Firebase
+//data structure.
+usersRef = lenderAppRef.child("users");
+itemsRef = lenderAppRef.child("items");
+trackersRef = lenderAppRef.child("trackers");
+
+//User() object constructor.
+function User(userIdent) {
+  this.userIdent = userIdent;
+  //inventory array contains UPCs of Item() objects created and owned by the user.
+  this.inventory = [];
+  //ledger object contains arrays of transaction IDs
+  this.ledger = {
+    //lent array contains transaction IDs of Tracker() objects in which they are lender.
+    lent: [],
+    //borrowed array contains transaction IDs of Tracker() objects in which they are borrower.
+    borrowed: []
+  };
+  //Keeps track of the individual user's reference in the Firebase.
+  this.userRef = usersRef.child(this.userIdent);
+}
+
+//Creates a node in Firebase for the given user.
+User.prototype.initialize = function () {
+  this.userRef.child("userIdent").set(this.userIdent);
+}
+
+//Creates a new Item() object, adds it to the user's inventory browser side
+//and on Firebase.
+User.prototype.createItem = function(itemDetails) {
+  var upc, item;
+  upc = this.generateUPC(itemDetails);
+  item = new Item(this.userIdent, itemDetails);
+  //Adds UPC to user's inventory
+  this.inventory.push(upc);
+  //Overwrites Firebase inventory with local inventory.
+  this.userRef.child("inventory").set(this.inventory);
+  //Adds item to Firebase
+  itemsRef.child(upc).set(item);
+
+}
+
+
+//Generates a UPC for item, using the itemDetails and the userIdent converted to unicode and turned into
+//a string separated by '-'. Function can be changed later for more advanced UPC generation.
+User.prototype.generateUPC = function(itemDetails) {
+  var slug, unicode, upc;
+  slug = this.userIdent + itemDetails;
+  unicode = [];
+
+  for (var i=0; i < slug.length; i++) {
+    unicode.push(slug.charCodeAt(i));
+  }
+
+  upc = unicode.join("-");
+
+  return upc;
+}
+
+User.prototype.initializeLend = function() {
+
+}
+
+User.prototype.confirmReturn = function() {
+
+}
+
+User.prototype.confirmBorrow = function() {
+
+}
+
+User.prototype.returnItem = function() {
+
+}
+
+function Item(owner, itemDetails) {
+  this.owner = owner;
+  this.itemDetails = itemDetails;
+}
+
+function Polonius() {
+
+}
+
+//Loads a form into the body, and passes the function to be used with the form data.
+Polonius.prototype.loadForm = function(formName, functionToCall) {
+  var parseForm, thisPolonius;
+
+  parseForm = this.parseForm;
+  thisPolonius = this;
+
+  $.get(formName + '.html', function(data) {
+    $("body").html(data);
+    $("#" + formName + "_form").on("submit", function(event) {
+      event.preventDefault();
+      var parsedData = parseForm($(this).serialize());
+      functionToCall(thisPolonius, parsedData[0], parsedData[1], parsedData[2]);
+    });
+  });
+}
+
+//Parses the serialized data from a form, returns array of form values without their associated names.
+Polonius.prototype.parseForm = function(serializedData) {
+  dataArray = serializedData.split("&");
+  for (var i = 0; i < dataArray.length; i++) {
+    dataArray[i] = dataArray[i].slice(dataArray[i].indexOf("=") + 1);
+  }
+
+  return dataArray;
+}
+
+//Sets userIdent in local storage, sets Polonius() object's currentUser to a new User() object
+//and uses initialize() to add it to Firebase.
+Polonius.prototype.createNewUser = function(thisPolonius, userIdent) {
+  localStorage.setItem('lenderUserIdent', userIdent);
+
+  thisPolonius.currentUser = new User(userIdent);
+  thisPolonius.currentUser.initialize();
+}
+
+//creates new Item, using the currentUser User() object.
+Polonius.prototype.createNewItem = function(thisPolonius, itemDetails) {
+  thisPolonius.currentUser.createItem(itemDetails);
+}
+
+//Takes a userIdent string, pulls a user off of Firebase and sets it as the Polonius() object's currentUser property.
+Polonius.prototype.setUserFromFirebase = function(userIdent) {
+  //
+  usersRef.child(userIdent).once('value', $.proxy(function(userSnapshot) {
+    var user;
+    user = new User(userIdent);
+    if (userSnapshot.val()['inventory']) {
+      user.inventory = userSnapshot.val()['inventory'];
+    }
+    if (userSnapshot.val()['ledger']) {
+      user.ledger = userSnapshot.val()['ledger'];
+    }
+
+    user.userRef = usersRef.child(localStorage['lenderUserIdent']);
+
+    this.currentUser = user;
+
+  },this));
+}
+
+Polonius.prototype.setUserDropdown = function() {
+
+  usersRef.once('value', function(usersSnapshot) {
+    var user;
+    //Pulling names from Firebase
+    var usersFromFirebase = usersSnapshot.val();
+    var namesForDropdown = Object.keys(usersFromFirebase);
+    console.dir(namesForDropdown);
+    //Use names in object and adds to pulldown list
+    var startDDPopulate = document.getElementById('selectLoginID');
+    var options = namesForDropdown;
+
+    for(var i=0; i< options.length; i++) {
+      var opt = options[i];
+      var el = document.createElement('option');
+      el.textContent = opt;
+      el.value = opt;
+      startDDPopulate.appendChild(el);
+    }
+  });
+};
+
+var x = new Polonius();
+x.setUserDropdown();
+
